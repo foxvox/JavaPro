875e16fee5e3d97003a882d19c5bfc7a801931fd
alex <windev@nate.com>
2024-03-13 21:13:51 +0900

    자바클래스사용예제

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+class Clss
+{
+    public static int suja = 100;
+
+    public static void method()
+    {
+        System.out.println("Java, Class Language!");
+
+        suja += 10;
+    }
+}
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-    }
+       int n = Clss.suja;
+
+       System.out.println(n);
+
+       Clss.method();
+
+       n = Clss.suja;
+       System.out.println(n);
+
+    } // public static void main(String[] args)
 }
