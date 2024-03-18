d8b50926badf72a87534b7428e4bf06f89fe6a9e
alex <windev@nate.com>
2024-03-19 03:40:40 +0900

    string ex

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
-class Clss
-{
-    public static void method(int[] x, int y)
-    {
-        for (int i = 0; i < x.length; i++)
-        {
-            x[i] += y;
-        }
-
-        // y = 123;
-    }
-}
-
 public class Main
 {
     public static void main(String[] args)
     {
-        int[] a = { 1, 3, 5, 7, 9};
-        int b = 10;
 
-       Clss.method(a, b);
-
-       for (int i = 0; i < a.length; i++)
-           System.out.println("a[" + i + "]: " + a[i]);
-
-       System.out.println("\nb: " + b);
 
     } // public static void main(String[] args)
 }
---
+++ string출력.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String str1 = "자바";
+        System.out.print(str1);
+        String str2 = " 30일";
+        System.out.print(str2);
+        System.out.println(" 완성");
+
+        String str = str1 + str2 + " 완성";
+        System.out.println(str);
+
+        String girl = "여자";
+        String girlFriend = girl + "친구";
+        System.out.println(girlFriend);
+
+        String girlFriend2 = girl + "친구";
+        System.out.println(girlFriend2);
+    } // public static void main(String[] args)
+}
